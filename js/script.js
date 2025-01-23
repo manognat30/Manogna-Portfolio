08dbd51b16cd65a9475ff11f15cf134cd792a0ef
manognat30 <142117676+manognat30@users.noreply.github.com>
2025-01-22 20:00:02 -0500

    Updated Resume in script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ var typed = new Typed(".typing", {
 /* ============================== Open Resume in New Window ============================ */
 document.getElementById('my-resume-btn').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent default action
-    window.open('files/My_Resume.pdf', '_blank');
+    window.open('files/My_Resume (1).pdf', '_blank');
 });
 
 /* ============================== Modal Elements in Projects ========================== */
