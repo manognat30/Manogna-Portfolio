8225a09e26312c615a051f6b523232924424981d
manognat30 <142117676+manognat30@users.noreply.github.com>
2025-02-18 17:18:40 -0500

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ var typed = new Typed(".typing", {
 /* ============================== Open Resume in New Window ============================ */
 document.getElementById('my-resume-btn').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent default action
-    window.open('files/My_Resume (1).pdf', '_blank');
+    window.open('files/Rèsume.pdf', '_blank');
 });
 
 /* ============================== Modal Elements in Projects ========================== */
